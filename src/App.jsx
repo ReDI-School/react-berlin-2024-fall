06319196cdcd51d55cf7a49656a38320abba9830
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2024-09-30 22:25:24 +0200

    Lesson 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import "./App.css";
 //import Lesson1 from './lessons/Lesson1'
 // import Lesson3SpreadRest from "./lessons/Lesson3SpreadRest";
-import Lesson4JSX from "./lessons/Lesson4JSX";
+// import Lesson4JSX from "./lessons/Lesson4JSX";
+import {
+  HelloClassComponent,
+  HelloFunctionalComponent,
+  Props,
+  ParentComponent,
+  ChildComponent,
+  ClassBasedState,
+  ExampleOfUseState,
+} from "./lessons/Lesson5State";
+
+const fruits = ["Orange", "Apple", "Pinapple", "Melon"];
 
 function App() {
   return (
     <>
-      <Lesson4JSX />
+      {/* <HelloClassComponent /> */}
+      {/* <HelloFunctionalComponent /> */}
+      {/* <Props name={"students"} /> */}
+      {/* <Props items={fruits} /> */}
+      {/* <ParentComponent>
+        <ChildComponent first={"Sofia"} second={"Anna"} third={"Karin"} />
+      </ParentComponent> */}
+      {/* <ClassBasedState /> */}
+      {/* <ExampleOfUseState /> */}
     </>
   );
 }
