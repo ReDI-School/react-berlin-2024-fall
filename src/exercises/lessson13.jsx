4954bffd72fc98b388a976c98289e4bc9ab564c0
annamariapl <annamaria.wojtyga@gmail.com>
2024-11-12 19:51:15 +0100

    cat-ercise [exercise about cats]

---
+++ src/exercises/lessson13.jsx
@@ -0,0 +1,3 @@
+// [] implement fetch to get cat facts and save cat fact in state variable
+// [] now instead of getting a cat fact on click, make them appear every 3 secend
+// [] add input that will allow user to alter frequency of displaying cat facts the amount of secocond 
